c1c144ad0a4dafb7e97772662d3de35f7beabd82
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-08-05 17:18:10 +0300

    questions app, auth, firebase

---
+++ src/auth.js
@@ -0,0 +1,33 @@
+export function getAuthForm() {
+    return( `
+    <form class="mui-form" id="auth-form">
+                <div class="mui-textfield mui-textfield--float-label">
+                    <input type="email" id="email" required>
+                    <label for="email">Email</label>
+                </div>
+                <div class="mui-textfield mui-textfield--float-label">
+                    <input type="password" id="password" required>
+                    <label for="password">Пароль</label>
+                </div>
+                <button type="submit"
+                     
+                        class="mui-btn mui-btn--raised mui-btn--primary">
+                    Войти
+                </button>
+            </form>
+    `)
+}
+
+export function authWithEmailAndPassword(email, password) {
+    const apiKey = "AIzaSyCpEQ3XtrGn8XLVTDNKFKP6E7Jt5Q6Skyo"
+    return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}
+`, {
+        method: 'POST',
+        body:JSON.stringify({email, password, returnSecureToken: true}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => data.idToken)
+}
